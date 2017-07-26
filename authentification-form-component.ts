8db0098957ab6a5a3151aff20769a0e51a21a2be
Leo Lanese <developer@leolanese.com>
2017-07-26 14:48:31 +0100

    Create authentification-form-component.ts

---
+++ authentification-form-component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'login-form',
+  templateUrl: './login-form.template.html'
+})
+export class LoginFormComponent {
+
+  private loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  @Input()
+  email: string;
+
+  @Output()
+  submitted = new EventEmitter();
+
+  ngOnChanges(change) {
+    if (change.email) {
+      this.loginForm.controls['email'].setValue(change.email.currentValue);
+    }
+  }
+
+  onSubmit({ email, password }) {
+    this.submitted.emit({ email, password });
+  }
+}
