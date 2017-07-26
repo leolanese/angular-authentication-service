06c4b6ea2b08db32fec680f19675492fa0cb882e
Leo Lanese <developer@leolanese.com>
2017-07-26 15:31:37 +0100

    Update authentification-form-component.specs.ts

--- authentification-form-component.specs.ts
+++ authentification-form-component.specs.ts
@@ -1,29 +1,30 @@
-import { LoginFormComponent } from './login-form.component';
+import { LoginAuthentificationFormComponent } from './login-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TestBed, inject, async } from '@angular/core/testing';
 import { Component } from '@angular/core';
 
 describe('Authentification Test:', () => {
-  let loginFormComponent: LoginFormComponent;
+  let loginFormComponent: LoginAuthentificationFormComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LoginFormComponent],
+      providers: [LoginAuthentificationFormComponent],
       imports: [FormsModule, ReactiveFormsModule]
     });
   });
 
   // inject: It injects services into the test function where you can alter, spy on, and manipulate them.
-  beforeEach(inject([LoginFormComponent], (loginForm: LoginFormComponent) => {
+  beforeEach(inject([LoginAuthentificationFormComponent], (loginForm: LoginAuthentificationFormComponent) => {
     loginFormComponent = loginForm;
   }));
 
   it('Should contain login data value on submit:', () => {
-    loginFormComponent.submitted.subscribe(({ email, password }) => {
+    loginFormComponent.submittingValues.subscribe(({ email, password }) => {
       expect(email).toEqual(expectedEmail);
       expect(password).toEqual(expectedPassword);
     });
 
     loginFormComponent.onSubmit({ email: expectedEmail, password: expectedPassword });
   });
+  
 });
